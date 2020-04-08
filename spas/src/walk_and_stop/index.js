e56b436e9e10ef802887613b01a12ad984fed940
z_xueqin <z_xueqin@icloud.com>
2020-04-08 19:39:21 +0000

    20.0002F walk

--- spas/src/walk_and_stop/index.js
+++ spas/src/walk_and_stop/index.js
@@ -1,5 +1,5 @@
 import { App } from './app.js'
-import { AppData } from './.local.appData.js';
+import { AppData } from './appData.js';
 
 window.onload = async () => {
     const app = new App(document.getElementById('app'), window.appData || AppData);
