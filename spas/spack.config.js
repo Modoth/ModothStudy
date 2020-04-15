2e049adf59227a33b156158360c55db9e9082452
z_xueqin <z_xueqin@icloud.com>
2020-04-15 11:55:59 +0000

    20.0014F

--- spas/spack.config.js
+++ spas/spack.config.js
@@ -1,64 +1,67 @@
-const path = require('path');
-const fs = require('fs');
+const path = require('path')
+const fs = require('fs')
 
 class FileUtils {
-    /**
-     * 
-     * @param {string} file 
-     * @return { string[] }
-     */
-    static readdir(file) {
-        return new Promise((resolve, reject) => {
-            fs.readdir(file, (err, files) => {
-                if (err) {
-                    reject(err)
-                } else {
-                    resolve(files);
-                }
-            });
-        });
-    }
+  /**
+   *
+   * @param {string} file
+   * @return { string[] }
+   */
+  static readdir(file) {
+    return new Promise((resolve, reject) => {
+      fs.readdir(file, (err, files) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(files)
+        }
+      })
+    })
+  }
 
-    static exists(file) {
-        return new Promise(resolve => {
-            fs.exists(file, resolve);
-        });
-    }
+  static exists(file) {
+    return new Promise((resolve) => {
+      fs.exists(file, resolve)
+    })
+  }
 }
-
 
 const getLocalConfigs = async () => {
-    const srcFolder = path.join(__dirname, 'src');
-    const subfolders = await FileUtils.readdir(srcFolder);
-    let entries = {};
-    let localCfg;
-    let localCfgFile = './.local.spack.config.js';
-    if (localCfgFile && await FileUtils.exists(localCfgFile)) {
-        try {
-            localCfg = require(localCfgFile);
-        }
-        catch  {
-            console.log(`Config File Error: ${localCfgFile}`);
-        }
+  const srcFolder = path.join(__dirname, 'src')
+  const subfolders = await FileUtils.readdir(srcFolder)
+  let entries = {}
+  let localCfg
+  let localCfgFile =
+    process.env['SPACK_CONFIG_FILE'] || './.local.spack.config.js'
+  if (localCfgFile && (await FileUtils.exists(localCfgFile))) {
+    try {
+      localCfg = require(localCfgFile)
+    } catch {
+      console.log(`Config File Error: ${localCfgFile}`)
     }
-    for (let subfolder of subfolders) {
-        const indexFile = path.join(srcFolder, subfolder, 'index.html');
-        if (!await FileUtils.exists(indexFile)) {
-            continue;
-        }
-        entries[subfolder] = Object.assign({ path: indexFile }, localCfg && localCfg.entries[subfolder]);
+  }
+  for (let subfolder of subfolders) {
+    const indexFile = path.join(srcFolder, subfolder, 'index.html')
+    if (!(await FileUtils.exists(indexFile))) {
+      continue
     }
-    return {
-        cd: (localCfg && localCfg.cd) || {},
-        entries
-    };
+    entries[subfolder] = Object.assign(
+      { path: indexFile },
+      localCfg && localCfg.entries[subfolder]
+    )
+  }
+  return {
+    cd: (localCfg && localCfg.cd) || {},
+    dist: localCfg.dist,
+    entries,
+  }
 }
 
-module.exports = getLocalConfigs().then(({ cd, entries }) => ({
-    entries,
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: '[name]'
-    },
-    cd
-}));
+module.exports = getLocalConfigs().then(({ cd, dist, entries }) => ({
+  entries,
+  output: {
+    path: path.join(__dirname, dist || 'dist'),
+    filename: '[name]',
+  },
+  cd,
+}))
