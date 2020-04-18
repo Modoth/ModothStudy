e9dda98367869ccbd7fd98559862d65634e3aa14
z_xueqin <z_xueqin@icloud.com>
2020-04-18 03:48:14 +0000

    20.0016F

--- spas/src/text-reader/app.js
+++ spas/src/text-reader/app.js
@@ -1,66 +1,7 @@
 import { Modal } from '../modal/index.js'
 import { readFile } from '../commons/readFile.js'
 import { TextRender } from './text-render.js'
-
-export class ResizeWatcher {
-  constructor(target) {
-    /**@type Window & typeof globalThis*/
-    this.mTarget = target
-    this.mTarget.onresize = (ev) => this.mOnResize(ev)
-    this.mResizeThreshold = 10
-    this.mCallbacks = []
-    this.mIsWatching = false
-    this.mCheckThreshold = 200
-  }
-  register(callback) {
-    callback && this.mCallbacks.push(callback)
-    if (!this.mIsWatching) {
-      this.mIsWatching = true
-      this.mLastCheckTime = 0
-      this.mLastW = this.mTarget.innerWidth
-      this.mLastH = this.mTarget.innerHeight
-    }
-  }
-
-  unregister(callback) {
-    if (!callback) {
-      return
-    }
-    const idx = this.mCallbacks.findIndex((c) => c === callback)
-    if (idx < 0) {
-      return
-    }
-    this.mCallbacks.splice(idx, 1)
-    if (this.mCallbacks.length === 0) {
-      this.mIsWatching = false
-    }
-  }
-
-  mOnResize(/**@type UIEvent*/ ev) {
-    if (!this.mIsWatching) {
-      return
-    }
-    const now = Date.now()
-    if (now - this.mLastCheckTime < this.mCheckThreshold) {
-      return
-    }
-    this.mLastCheckTime = now
-    let w = this.mTarget.innerWidth
-    let h = this.mTarget.innerHeight
-
-    if (
-      Math.abs(this.mLastW - w) < this.mResizeThreshold &&
-      Math.abs(this.mLastH - h) < this.mResizeThreshold
-    ) {
-      return
-    }
-    this.mLastW = w
-    this.mLastH = h
-    for (const callback of this.mCallbacks) {
-      callback(w, h)
-    }
-  }
-}
+import { ResizeWatcher } from '../commons/resize-watcher.js'
 
 export class App {
   constructor(window) {
