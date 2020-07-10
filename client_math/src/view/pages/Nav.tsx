f8d91044725f68d3ec52d8ee7c81fe978fadaae4
z_xueqin <z_xueqin@icloud.com>
2020-07-10 13:32:59 +0000

    auto

--- client_math/src/view/pages/Nav.tsx
+++ client_math/src/view/pages/Nav.tsx
@@ -3,12 +3,12 @@ import './Nav.less'
 import { Link } from 'react-router-dom'
 import { useUser, useServicesLocator } from '../../app/Contexts'
 import { Configs } from '../../apis'
-import { Menu, Avatar } from 'antd'
+import { Menu, Avatar, Drawer, Button } from 'antd'
 import {
   UserOutlined,
   HomeOutlined,
   ApiOutlined,
-  ApartmentOutlined,
+  MenuOutlined,
   SettingOutlined,
   TeamOutlined,
   TagsOutlined
@@ -23,7 +23,8 @@ function Nav() {
   const locator = useServicesLocator()
   const plugin = locator.locate(IPluginInfo)
   const langs = locator.locate(ILangsService)
-  const [title,setTitile] = useState(langs.get(Configs.UiLangsEnum.Home))
+  const [title, setTitile] = useState(langs.get(Configs.UiLangsEnum.Home))
+  const [showDrawer, setShowDrawer] = useState(false)
   const user = useUser()
   const fetchTitle = async () => {
     var titles = await locator.locate(ITagsService).getValues(TagNames.TitleTag)
@@ -37,54 +38,62 @@ function Nav() {
     fetchTitle()
   }, [])
   return (
-    <Menu mode="horizontal" className={classNames("nav", generateRandomStyle())}>
-      <Menu.Item icon={<HomeOutlined />}>
-        <Link to="/">{title}</Link>
-      </Menu.Item>
-      {
-        plugin.types.map(t => <Menu.Item key={t.route} icon={t.icon}>
-          <Link to={'/' + t.route}>{t.name}</Link>
-        </Menu.Item>)
-      }
-      <Menu.Item disabled={true} className="menu-item-devider"></Menu.Item>
-      {user && user.managePermission ? (
-        <SubMenu
-          icon={<SettingOutlined />}
-          title={langs.get(Configs.UiLangsEnum.Manage)}
-        >
-          <Menu.Item icon={<ApiOutlined />}>
-            <Link to="/manage/subjects">
-              {langs.get(Configs.UiLangsEnum.Subject)}
-            </Link>
-          </Menu.Item>
-          <Menu.Item icon={<TagsOutlined />}>
-            <Link to="/manage/tags">{langs.get(Configs.UiLangsEnum.Tags)}</Link>
-          </Menu.Item>
-          <Menu.Item icon={<TeamOutlined />}>
-            <Link to="/manage/users">
-              {langs.get(Configs.UiLangsEnum.User)}
-            </Link>
-          </Menu.Item>
-        </SubMenu>
-      ) : null}
-      {user ? (
-        <Menu.Item
-          className="nav-avatar-icon"
-          icon={
-            <Avatar
-              className="avatar"
-              src={user.avatar || '/assets/avatar.png'}
-            />
+    <>
+      <Drawer className="side-nav-panel" visible={showDrawer} onClose={() => setShowDrawer(false)} closable={false} placement="left">
+        <Menu className="side-nav" mode="inline" onClick={() => setShowDrawer(false)} >
+          {
+            plugin.types.map(t => <Menu.Item key={t.route} icon={t.icon}>
+              <Link to={'/' + t.route}>{t.name}</Link>
+            </Menu.Item>)
           }
-        >
-          <Link to="/account"></Link>
+          {user && user.managePermission ? (
+            <SubMenu
+              icon={<SettingOutlined />}
+              title={langs.get(Configs.UiLangsEnum.Manage)}
+            >
+              <Menu.Item icon={<ApiOutlined />}>
+                <Link to="/manage/subjects">
+                  {langs.get(Configs.UiLangsEnum.Subject)}
+                </Link>
+              </Menu.Item>
+              <Menu.Item icon={<TagsOutlined />}>
+                <Link to="/manage/tags">{langs.get(Configs.UiLangsEnum.Tags)}</Link>
+              </Menu.Item>
+              <Menu.Item icon={<TeamOutlined />}>
+                <Link to="/manage/users">
+                  {langs.get(Configs.UiLangsEnum.User)}
+                </Link>
+              </Menu.Item>
+            </SubMenu>
+          ) : null}
+        </Menu>
+      </Drawer>
+
+      <Menu mode="horizontal" className={classNames("nav", generateRandomStyle())}>
+        <Menu.Item icon={<MenuOutlined />} onClick={() => setShowDrawer(true)}>
         </Menu.Item>
-      ) : (
-          <Menu.Item icon={<UserOutlined />}>
-            <Link to="/login">{langs.get(Configs.UiLangsEnum.Login)}</Link>
+        <Menu.Item className="nav-home" icon={<HomeOutlined />}>
+          <Link to="/">{title}</Link>
+        </Menu.Item>
+        {user ? (
+          <Menu.Item
+            className="nav-avatar-icon"
+            icon={
+              <Avatar
+                className="avatar"
+                src={user.avatar || '/assets/avatar.png'}
+              />
+            }
+          >
+            <Link to="/account"></Link>
           </Menu.Item>
-        )}
-    </Menu>
+        ) : (
+            <Menu.Item icon={<UserOutlined />}>
+              <Link to="/login">{langs.get(Configs.UiLangsEnum.Login)}</Link>
+            </Menu.Item>
+          )}
+      </Menu>
+    </>
   )
 }
 
